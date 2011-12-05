05b901ad623a1a4e23d40bbff1ad9424cda78da7
Benedikt Terhechte <terhechte@gmail.com>
2011-12-05 10:59:49 +0100

    completed tasks, removed nokogiri

--- _plugins/helpers.rb
+++ _plugins/helpers.rb
@@ -39,32 +39,5 @@ module Liquid
 
   end
 
-  module NokogiriTruncator
-    module NodeWithChildren
-      def truncate(max_length)
-        return self if inner_text.length <= max_length
-        truncated_node = self.dup
-        truncated_node.children.remove
-
-        self.children.each do |node|
-          remaining_length = max_length - truncated_node.inner_text.length
-          break if remaining_length <= 0
-          truncated_node.add_child node.truncate(remaining_length)
-        end
-        truncated_node
-      end
-    end
-
-    module TextNode
-      def truncate(max_length)
-        Nokogiri::XML::Text.new(content[0..(max_length - 1)], parent)
-      end
-    end
-
-  end
-  Nokogiri::HTML::DocumentFragment.send(:include, NokogiriTruncator::NodeWithChildren)
-  Nokogiri::XML::Element.send(:include, NokogiriTruncator::NodeWithChildren)
-  Nokogiri::XML::Text.send(:include, NokogiriTruncator::TextNode)
-
   Liquid::Template.register_filter(ExtendedFilters)
 end
