a160c32bc1bb177820df4ed943ef1e721f4901ee
Benedikt Terhechte <terhechte@gmail.com>
2011-12-05 11:00:53 +0100

    removed more nokogiri

--- _plugins/helpers.rb
+++ _plugins/helpers.rb
@@ -1,6 +1,3 @@
-require 'hpricot'
-require 'nokogiri'
-
 module Liquid
   
   module ExtendedFilters
@@ -15,14 +12,6 @@ module Liquid
 
     def date_to_utc(input)
       input.getutc
-    end
-
-    def html_truncatewords(text, max_length = 200, ellipsis = "")
-      ellipsis_length = ellipsis.length     
-      doc = Nokogiri::HTML::DocumentFragment.parse text
-      content_length = doc.inner_text.length
-      actual_length = max_length - ellipsis_length
-      content_length > actual_length ? doc.truncate(actual_length).inner_html + ellipsis : text.to_s
     end
 
     def preview(text, delimiter = '<!-- end_preview -->')
